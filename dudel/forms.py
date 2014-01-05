4f743d4164c0e0fbb4b3e64bfb8416fdd628953e
Henning Pridöhl <8pridoeh@informatik.uni-hamburg.de>
2014-01-05 18:03:15 +0100

    Fix LDAP injection

--- dudel/forms.py
+++ dudel/forms.py
@@ -8,6 +8,7 @@ from wtforms.ext.dateutil.fields import DateTimeField
 from wtforms.validators import Required, Length, Regexp, Optional, NoneOf
 from dudel.models import Poll, update_user_data
 import ldap
+from ldap.dn import escape_dn_chars
 
 # Helper class for multiple forms on one page
 class MultiForm(Form):
@@ -59,7 +60,7 @@ class LDAPAuthenticator(object):
             # At this point, we update the user info (since we already have
             # a connection to LDAP)
             #connection.start_tls_s()
-            connection.simple_bind_s(app.config["LDAP_DN"] % username, field.data)
+            connection.simple_bind_s(app.config["LDAP_DN"] % escape_dn_chars(username), field.data)
             filter = app.config["LDAP_FILTER"] % "13demo1"
             results = connection.search_s(app.config["LDAP_BASEDN"], ldap.SCOPE_SUBTREE, filter)
             results = {k:(v if len(v)>1 else v[0]) for k,v in results[0][1].iteritems()}
