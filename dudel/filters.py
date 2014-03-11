9b698df62b123f3bc10f242bab4afbaf74822d1d
Damian Hofmann <2hofmann@informatik.uni-hamburg.de>
2014-03-11 19:15:14 +0100

    Localize dates and times

--- dudel/filters.py
+++ dudel/filters.py
@@ -1,18 +1,21 @@
 from dudel import app, ICONS
 from dudel.forms import LoginForm, LanguageForm
 from flask import g
+from flask.ext.babel import format_date, format_time, get_locale
+
+date_formats = {'de': 'EEE, dd. MMM'}
 
 @app.template_filter()
 def date(s):
-    return s.strftime("%a %d %b")
+    return format_date(s, date_formats.get(get_locale().language, 'EEE, dd MMM'))
 
 @app.template_filter()
 def time(s):
-    return s.strftime("%H:%M")
+    return format_time(s, 'HH:mm')
 
 @app.template_filter()
 def datetime(s):
-    return s.strftime("%a %d %b %H:%M")
+    return date(s) + ', ' + time(s)
 
 @app.template_filter()
 def voter(vote):
