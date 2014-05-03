93c4e809f5e81db47865f111f489f596daa9faef
Damian Hofmann <2hofmann@informatik.uni-hamburg.de>
2014-05-03 17:46:42 +0200

    Do not convert the time of poll choices to local time
    All users should see the times in a poll that were
    originally entered by its creator. Localizing poll times
    by default could lead to confusion if voters come from
    different time zones. (Maybe add an option later to enable
    localization for specific polls, if desired.)

--- dudel/filters.py
+++ dudel/filters.py
@@ -6,16 +6,18 @@ from flask.ext.babel import format_date, format_time, get_locale
 date_formats = {'de': 'EEE, dd. MMM'}
 
 @app.template_filter()
-def date(s):
-    return format_date(s, date_formats.get(get_locale().language, 'EEE, dd MMM'))
+def date(s, rebase=True):
+    return format_date(s,
+                       date_formats.get(get_locale().language, 'EEE, dd MMM'),
+                       rebase=rebase)
 
 @app.template_filter()
-def time(s):
-    return format_time(s, 'HH:mm')
+def time(s, rebase=True):
+    return format_time(s, 'HH:mm', rebase=rebase)
 
 @app.template_filter()
-def datetime(s):
-    return date(s) + ', ' + time(s)
+def datetime(s, rebase=True):
+    return date(s, rebase) + ', ' + time(s, rebase)
 
 @app.template_filter()
 def voter(vote):
