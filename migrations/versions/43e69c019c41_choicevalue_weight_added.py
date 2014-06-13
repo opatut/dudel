a47d1f43eea74c7415f3633098d0a6a30c5a8f48
Paul Bienkowski <opatutlol@aol.com>
2014-06-13 23:21:53 +0200

    Fixed recent migration for postgresql

--- migrations/versions/43e69c019c41_choicevalue_weight_added.py
+++ migrations/versions/43e69c019c41_choicevalue_weight_added.py
@@ -16,9 +16,9 @@ import sqlalchemy as sa
 def upgrade():
     op.add_column('choice_value', sa.Column('weight', sa.Float, server_default="0", nullable=False))
     # set default weights for existing default choice_values (yes/no/maybe)
-    op.execute("""UPDATE choice_value SET weight = 0;""")
-    op.execute("""UPDATE choice_value SET weight = 0.5 WHERE title = "maybe";""")
-    op.execute("""UPDATE choice_value SET weight = 1 WHERE title = "yes";""")
+    op.execute("UPDATE choice_value SET weight = 0;")
+    op.execute("UPDATE choice_value SET weight = 0.5 WHERE title = 'maybe';")
+    op.execute("UPDATE choice_value SET weight = 1 WHERE title = 'yes';")
 
 def downgrade():
     op.drop_column('choice_value', 'weight')
