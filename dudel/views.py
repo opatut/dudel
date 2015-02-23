f3a6069eafe0dd04eeaa445bddbf64dd675eebbd
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2015-02-23 15:50:17 +0100

    better errorhandling

--- dudel/views.py
+++ dudel/views.py
@@ -1031,4 +1031,8 @@ def root(locale):
 @app.errorhandler(401)
 @app.errorhandler(400)
 def error(err):
-    return render_template("error.html", error=err), err.code
+    # if it is not a simple 4xx send a message to sentry
+    if not hasattr(err, 'code') or err.code not in [404, 403, 401, 400]:
+        sentry.captureException()
+    return render_template("error.html", error=err), err.code if hasattr(err, 'code') else 500
+
