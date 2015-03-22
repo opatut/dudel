7a20cc0f0d693c5686168e0b4f7fe292cfb3bab5
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2015-03-22 14:00:52 +0100

    Possible DDOS: Refactor /api/cron to a cli command

--- manage.py
+++ manage.py
@@ -1,5 +1,36 @@
 #!/usr/bin/env python2
-from dudel import manager
+from __future__ import print_function
+import sys
+from dudel import manager, db
+from dudel.models import Poll
+
+
+@manager.command
+def cron():
+    # This view should execute some regular tasks to be called by a cronjob, e.g.
+    # by simply curl-ing "/api/cron"
+
+    # Update LDAP groups
+    from dudel.plugins.ldapauth import ldap_connector
+
+    try:
+        ldap_connector.update_users()
+        print("updated LDAP users\n")
+    except Exception, e:
+        print("error updating LDAP users: %s\n" % str(e), file=sys.stderr)
+
+    try:
+        ldap_connector.update_groups()
+        print("updated LDAP groups\n")
+    except Exception, e:
+        print("error updating LDAP groups: %s\n" % str(e), file=sys.stderr)
+
+    for poll in Poll.query.filter_by(deleted=False).all():
+        if poll.should_auto_delete:
+            poll.deleted = True
+            print("auto-deleted poll: %s\n" % poll.title)
+
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
