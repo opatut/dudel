4ab6d45e50a1798c5135930b6717f43560e41f25
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2015-03-22 14:05:25 +0100

    cron: update ldap only if it is enabled

--- manage.py
+++ manage.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python2
 from __future__ import print_function
 import sys
-from dudel import manager, db
+from dudel import manager, db, app
 from dudel.models import Poll
 
 
 @manager.command
 def cron():
-    # This view should execute some regular tasks to be called by a cronjob, e.g.
-    # by simply curl-ing "/api/cron"
+    """
+    This view should execute some regular tasks to be called by a cronjob, e.g.
+    by simply calling "./manage.py cron"
+    """
 
-    # Update LDAP groups
-    from dudel.plugins.ldapauth import ldap_connector
+    if "ldap" in app.config['LOGIN_PROVIDERS']:
+        # Update LDAP only if it is enabled
+        from dudel.plugins.ldapauth import ldap_connector
 
-    try:
-        ldap_connector.update_users()
-        print("updated LDAP users\n")
-    except Exception, e:
-        print("error updating LDAP users: %s\n" % str(e), file=sys.stderr)
+        try:
+            ldap_connector.update_users()
+            print("updated LDAP users\n")
+        except Exception, e:
+            print("error updating LDAP users: %s\n" % str(e), file=sys.stderr)
 
-    try:
-        ldap_connector.update_groups()
-        print("updated LDAP groups\n")
-    except Exception, e:
-        print("error updating LDAP groups: %s\n" % str(e), file=sys.stderr)
+        try:
+            ldap_connector.update_groups()
+            print("updated LDAP groups\n")
+        except Exception, e:
+            print("error updating LDAP groups: %s\n" % str(e), file=sys.stderr)
 
     for poll in Poll.query.filter_by(deleted=False).all():
         if poll.should_auto_delete:
