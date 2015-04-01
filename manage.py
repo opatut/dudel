a50500eb7d369adbf7bcd83d7e94bd96a674fea5
Paul Bienkowski <paul.bienkowski@sinnerschrader.com>
2015-04-01 10:42:33 +0200

    Added verbose flag to cronjob

--- manage.py
+++ manage.py
@@ -6,32 +6,47 @@ from dudel.models import Poll
 
 
 @manager.command
-def cron():
+def cron(verbose=False):
     """
     This view should execute some regular tasks to be called by a cronjob, e.g.
     by simply calling "./manage.py cron"
     """
+
+    def stdout(*args, **kwargs):
+        if not verbose: return
+        if 'end' not in kwargs: kwargs['end'] = '\n'
+        print(*args, **kwargs)
+
+    def stderr(*args, **kwargs):
+        if 'end' not in kwargs: kwargs['end'] = '\n'
+        kwargs['file'] = sys.stderr
+        print(*args, **kwargs)
+
 
     if "ldap" in app.config['LOGIN_PROVIDERS']:
         # Update LDAP only if it is enabled
         from dudel.plugins.ldapauth import ldap_connector
 
         try:
+            stdout("Updating LDAP users... ", end="")
             ldap_connector.update_users()
-            print("updated LDAP users\n")
+            stdout("[DONE]")
         except Exception, e:
-            print("error updating LDAP users: %s\n" % str(e), file=sys.stderr)
+            stdout("[FAILED]")
+            stderr("Error updating LDAP users: %s" % str(e))
 
         try:
+            stdout("Updating LDAP groups... ", end="")
             ldap_connector.update_groups()
-            print("updated LDAP groups\n")
+            stdout("[DONE]")
         except Exception, e:
-            print("error updating LDAP groups: %s\n" % str(e), file=sys.stderr)
+            stdout("[FAILED]")
+            stderr("Error updating LDAP groups: %s" % str(e))
 
     for poll in Poll.query.filter_by(deleted=False).all():
         if poll.should_auto_delete:
             poll.deleted = True
-            print("auto-deleted poll: %s\n" % poll.title)
+            stdout("Auto-deleted poll: %s" % poll.title)
 
     db.session.commit()
 
